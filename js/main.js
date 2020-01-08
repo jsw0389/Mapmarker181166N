2e2bad6f97344702ec8969c115ae5a4ae0ce1dcf
jsw0389 <46648772+jsw0389@users.noreply.github.com>
2020-01-08 23:13:35 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -50,72 +50,21 @@ function searchAddress() {
 
 		var result = response.v2, // 검색 결과의 컨테이너
 				items = result.addresses; // 검색 결과의 배열
+
+				var contentString = ['v'].join('');
+
+				var infowindow = new naver.maps.InfoWindow({
+				    content: contentString
+				});
+		});
+
+		/*
+		marker.setPosition(tempCoords);
+		marker.setMap(map);
+		map.setCenter(tempCoords);
+		map.setLevel(3);
+		*/
 	});
 }
-
-var CustomOverlay = function(options) {
-    this._element = $('<div style="position:absolute;left:0;top:0;width:124px;background-color:#F2F0EA;text-align:center;border:2px solid #6C483B;">' +
-                        '<span style="font-weight: bold;"> Brown </span>' +
-                        '</div>')
-
-    this.setPosition(options.position);
-    this.setMap(options.map || null);
-};
-
-CustomOverlay.prototype = new naver.maps.OverlayView();
-CustomOverlay.prototype.constructor = CustomOverlay;
-
-CustomOverlay.prototype.setPosition = function(position) {
-    this._position = position;
-    this.draw();
-};
-
-CustomOverlay.prototype.getPosition = function() {
-    return this._position;
-};
-
-CustomOverlay.prototype.onAdd = function() {
-    var overlayLayer = this.getPanes().overlayLayer;
-
-    this._element.appendTo(overlayLayer);
-};
-
-CustomOverlay.prototype.draw = function() {
-    if (!this.getMap()) {
-        return;
-    }
-
-    var projection = this.getProjection(),
-        position = this.getPosition(),
-        pixelPosition = projection.fromCoordToOffset(position);
-
-    this._element.css('left', pixelPosition.x);
-    this._element.css('top', pixelPosition.y);
-};
-
-CustomOverlay.prototype.onRemove = function() {
-    var overlayLayer = this.getPanes().overlayLayer;
-
-    this._element.remove();
-    this._element.off();
-};
-
-var position = new naver.maps.LatLng(37.3849483, 127.1229117);
-var map = new naver.maps.Map("map", {
-    center: position,
-    zoom: 14
-});
-var overlay = new CustomOverlay({
-    map: map,
-    position: position
-});
-
-naver.maps.Event.addListener(map, 'click', function(e) { //클릭한 위치에 오버레이를 추가합니다.
-    var overlay = new CustomOverlay({
-        position: e.coord
-    });
-
-    overlay.setMap(map);
-});
 
 map.setCursor('pointer');
